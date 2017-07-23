90c2f3f2aca41160562ddeca9803b898c1d2c6e6
shanemat <shanemat@fit.cvut.cz>
2017-07-23 22:20:47 +0200

    Core library - Global enums added

---
+++ core-library/src/main/java/cz/cvut/fit/mobchar/core_library/enums/Extras.java
@@ -0,0 +1,12 @@
+package cz.cvut.fit.mobchar.core_library.enums;
+
+/**
+ * Created by shanemat (FIT-CTU) on 23.7.2017 as class holding
+ * information about extras for starting modules.
+ */
+
+public class Extras {
+
+    public static final String EXTRA_ADVENTURE_ID = "cz.cvut.fit.mobchar.adventure_id";
+    public static final String EXTRA_CHARACTER_ID = "cz.cvut.fit.mobchar.character_id";
+}
---
+++ core-library/src/main/java/cz/cvut/fit/mobchar/core_library/enums/Tables.java
@@ -0,0 +1,23 @@
+package cz.cvut.fit.mobchar.core_library.enums;
+
+/**
+ * Created by shanemat (FIT-CTU) on 23.7.2017 as class storing names
+ * of columns of tables in databases across the app.
+ */
+
+public class Tables {
+
+    public class DrDPJ {
+        public class AdventureTable {
+            public static final String ID = "id";
+            public static final String NAME = "name";
+        }
+    }
+
+    public class DrDPlayer {
+        public class CharacterTable {
+            public static final String ID = "_id";
+            public static final String NAME = "name";
+        }
+    }
+}

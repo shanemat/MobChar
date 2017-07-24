5b45bc20789aa81791fb4e68ff1b723116e98082
shanemat <shanemat@fit.cvut.cz>
2017-07-24 19:51:14 +0200

    Core - DAO factory added

---
+++ core/src/main/java/cz/cvut/fit/mobchar/core/data/DAOFactory.java
@@ -0,0 +1,9 @@
+package cz.cvut.fit.mobchar.core.data;
+
+/**
+ * Created by shanemat (FIT-CTU) on 24.7.2017 as object providing
+ * implementations of specific interfaces.
+ */
+
+public class DAOFactory {
+}

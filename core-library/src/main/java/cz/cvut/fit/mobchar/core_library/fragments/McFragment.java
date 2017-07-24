8bfe73ae7f8b1b1f9a6ae56b6de9b97eaf98218f
shanemat <shanemat@fit.cvut.cz>
2017-07-24 11:49:20 +0200

    Core library - Base fragment added

---
+++ core-library/src/main/java/cz/cvut/fit/mobchar/core_library/fragments/McFragment.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.mobchar.core_library.fragments;
+
+import android.support.v4.app.Fragment;
+
+import cz.cvut.fit.mobchar.core_library.interfaces.IFReloadable;
+
+/**
+ * Created by shanemat (FIT-CTU) on 24.7.2017 as abstract Fragment implementing
+ * IFReloadable.
+ */
+
+public abstract class McFragment extends Fragment implements IFReloadable {
+}

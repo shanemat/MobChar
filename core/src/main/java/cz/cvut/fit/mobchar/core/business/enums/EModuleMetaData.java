a47e041a8f1e4cc099abc750677824fe6b43ee68
shanemat <shanemat@fit.cvut.cz>
2017-07-27 14:51:27 +0200

    Core - Module meta data specified into Enum

---
+++ core/src/main/java/cz/cvut/fit/mobchar/core/business/enums/EModuleMetaData.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.mobchar.core.business.enums;
+
+/**
+ * Created by shanemat (FIT-CTU) on 25.7.2017 as class holding constants
+ * for localization of module's meta-data.
+ */
+
+public class EModuleMetaData {
+
+    public static final String META_MODULE_NAME = "module_name";
+    public static final String META_MODULE_ABBREVIATION = "module_abbreviation";
+    public static final String META_MODULE_PROVIDER_URI = "provider_uri";
+}
